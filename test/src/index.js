7a4f0ebaa0a771635cd5ffc30a02e3f15dbd318f
DAB0mB <emanor6@gmail.com>
2016-05-07 18:54:20 +0300

    Add test skelton

---
+++ test/src/index.js
@@ -0,0 +1,11 @@
+import './specs/loader.spec.js';
+import './specs/module-helpers/injectable.spec';
+import './specs/module-helpers/directive.spec';
+import './specs/module-helpers/controller.spec';
+import './specs/module-helpers/filter.spec';
+import './specs/module-helpers/config.spec';
+import './specs/module-helpers/factory.spec';
+import './specs/module-helpers/service.spec';
+import './specs/module-helpers/provider.spec';
+import './specs/module-helpers/runner.spec';
+import './specs/module-helpers/decorator.spec';
---
+++ test/webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  entry: [
+    __dirname + '/src/index.js'
+  ],
+  output: {
+    path: __dirname + '/dist',
+    filename: 'test.bundle.js'
+  },
+  target: 'web',
+  devtool: 'source-map',
+  babel: {
+    presets: ['es2015', 'stage-0'],
+    plugins: ['add-module-exports']
+  },
+  module: {
+    loaders: [{
+      test: /test\/src\/.*\.js$/,
+      loader: 'babel'
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js'],
+    aliases: {
+      'angular-ecmascript': __dirname + '/..'
+    }
+  }
+};
